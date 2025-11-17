f096948f1f57bd92e9288e0f398f9d13e295a28d
KareemZafan <kareemm.zafan@gmail.com>
2025-11-17 11:51:06 +0200

    Adding new add function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+double add(double a , double b)
+{
+   return a + b ;
+} 
 
 int main(){
 
 printf("Hellow World!"); 
+printf("Sum(%f,%f) = %f",10,20,add(10,20)); 
+return 0;
 
-return 0;}
+}
